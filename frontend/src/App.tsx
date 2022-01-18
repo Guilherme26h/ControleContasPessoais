23d34b50ccc2a415355a8d33dd7d2e9236ced9bd
Guilherme Henrique de Sousa Jesus <guilherme26h@gmail.com>
2022-01-18 07:08:47 -0300

    Footer finalizado faltando somente os icones

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 
 
 function App() {
 
   return (
-    <NavBar />
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
   );
 }
 
